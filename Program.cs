0ee499fabd8d9471b3867be765961a468ec85cf2
Thomas-Casey <102041956@student.swin.edu.au>
2021-04-19 03:02:42 +1000

    first

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arraystask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = new string[5];
+
+            int index = 0;
+
+            while (index < names.Length)
+            {
+                System.Console.WriteLine("Please Enter Name {0}", index + 1);
+                names[index] = System.Console.ReadLine();
+                index++;
+            }
+            Console.WriteLine("=======================================");
+            foreach (string item in names)
+            {
+                Console.WriteLine("Hello " + item);
+            }
+        }
+    }
+}
